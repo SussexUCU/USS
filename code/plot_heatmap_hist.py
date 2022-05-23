06cbc240ebba1e820b40a93972049df4be8d56ed
Mark Hindmarsh <m.b.hindmarsh@sussex.ac.uk>
2022-05-23 09:48:37 +0300

    Added data digest to plotting/saving script. Added output csv files

--- code/plot_heatmap_hist.py
+++ code/plot_heatmap_hist.py
@@ -71,8 +71,8 @@ print("Peforming linear and geometric sums for total pension")
 pension_df['old_pension_total'] = pension_df['old_pension_pa']*n_years
 pension_df['new_pension_total_linear'] = (pension_df['new_pension_pa'] 
                                           + pension_df['new_pension_20yr_pa'])*n_years/2
-r = (pension_df['new_pension_20yr_pa']/pension_df['new_pension_pa'])**(1/n_years)
-pension_df['new_pension_total_geomet'] = pension_df['new_pension_pa']*(r**n_years - 1)/(r-1)
+r = (pension_df['new_pension_20yr_pa']/pension_df['new_pension_pa'])**(1/(n_years-1))
+pension_df['new_pension_total_geomet'] = pension_df['new_pension_pa']*(1 - r**n_years )/(1-r)
 
 
 geom_lin_diff = pension_df['new_pension_total_geomet'] - pension_df['new_pension_total_linear']
@@ -166,6 +166,20 @@ for df_by_cpi, cpi in zip(df_by_cpi_list, cpi_list):
     non_zero_nos_df_list.append(non_zero_nos_df)
 
 #%%
+# Now for salaries under 40k
+salary_thresh = 40000
+
+pension_df_under40k = pension_df[pension_df['salary'] < salary_thresh].copy()
+
+pension_df_under40k['binned_by_pc_loss'] = pd.cut(pension_df_under40k['loss_percent'], 
+                                               bins=pc_bin_edges, right=False, labels=pc_bin_labels)
+pension_df_under40k['inflation'] *= 100
+pension_df_under40k['inflation'] = pension_df_under40k['inflation'].round(1)
+
+pension_df_under40k_binned = bin_and_remove_zeros(pension_df_under40k.copy())
+
+pension_df_under40k_binned_grouped = pension_df_under40k_binned.groupby(['binned_by_pc_loss','inflation']).sum().unstack()
+#%%
 
 # Calculate pension losses for poersonas
 
@@ -191,16 +205,218 @@ def total_and_mean_losses(df):
     mean_loss_percent = losses_percent_wt.sum()
     return total_loss_pounds, mean_loss_pounds, mean_loss_percent
 
+def quartile_losses(df, key):
+    df_sorted = df.sort_values(key)
+    df_sorted["cum_number"] = df_sorted["number"].cumsum()
+    q_list = np.array([0.25,0.5,0.75])
+    loss_quartiles = np.zeros((len(q_list),))
+    num_quartiles = df_sorted["cum_number"].max() * q_list
+    for n, q in enumerate(q_list):
+        idx = np.abs(num_quartiles[n] - df_sorted["cum_number"]).argmin()
+        loss_quartiles[n] = df_sorted[key].iloc[idx]
+
+    return loss_quartiles
+
+# Assemble various quantities into a DataFrame
+
+losses_df = pd.DataFrame( [c/10 for c in cpi_list ], columns=["CPI"])
+
 # Total and mean loss
 
 print("Total and mean losses")
 
-for df in df_by_cpi_list:
+total_mean_losses = []
+for cpi, df in zip(cpi_list, df_by_cpi_list):
     total_loss_pounds, mean_loss_pounds, mean_loss_percent = total_and_mean_losses(df)
+    total_mean_losses.append([total_loss_pounds, mean_loss_pounds, mean_loss_percent])
     
-    print("Total loss GBP: {:.3} b".format(total_loss_pounds/1e9), 
-          "Mean loss GBP: {:.1f} k".format(mean_loss_pounds/1e3), 
-          "Mean loss %: {:.3f}".format(mean_loss_percent) )
+    print("CPI {:.1f}%  Total loss GBP: {:.3}b".format(cpi/10, total_loss_pounds/1e9), 
+          "CPI {:.1f}%  Mean loss GBP: {:.1f}k".format(cpi/10, mean_loss_pounds/1e3), 
+          "CPI {:.1f}%  Mean loss %: {:.3f}".format(cpi/10, mean_loss_percent) )
+
+tml_df = pd.DataFrame(total_mean_losses, 
+                      columns=["Total loss GBP", "Mean loss GBP", "Mean loss %"])
+
+losses_df = losses_df.join(tml_df)
+
+#%%
+print("Quartile losses (percent)")
+
+loss_percent_quartiles_list = []
+np.set_printoptions(precision=0)
+for cpi, df in zip(cpi_list, df_by_cpi_list):
+    loss_percent_quartiles = quartile_losses(df, "loss_percent")
+    loss_percent_quartiles_list.append(loss_percent_quartiles)
+    print("CPI {:.1f}%  Quartile losses %:   ".format(cpi/10), loss_percent_quartiles) 
+
+lpcq_df = pd.DataFrame(loss_percent_quartiles_list, 
+                       columns=["% loss Q1", "% loss Q2", "% loss Q3"])
+
+losses_df = losses_df.join(lpcq_df)
+
+#%%
+print("Quartile losses (pounds)")
+
+loss_pounds_quartiles_list = []
+np.set_printoptions(precision=1)
+for cpi, df in zip(cpi_list, df_by_cpi_list):
+    loss_pounds_quartiles = quartile_losses(df, "loss_pounds")
+    loss_pounds_quartiles_list.append(loss_pounds_quartiles)
+    print("CPI {:.1f}%  Quartile losses GBP:   ".format(cpi/10), loss_pounds_quartiles) 
+
+lgbpq_df = pd.DataFrame(loss_pounds_quartiles_list, 
+                        columns=["GBP loss Q1", "GBP loss Q2", "GBP loss Q3"])
+
+losses_df = losses_df.join(lgbpq_df)
+
+#%%
+# Lecturer A and B pay scale 2021
+# 43, 50296
+# 42, 48835
+# 41, 47419
+# 40, 46042
+# 39, 44706
+# 38, 43434
+# 37, 42149
+# 36, 40927
+# 35, 39739
+# 34, 38587
+# 33, 37467
+# 32, 36382
+# 31, 35326
+# 30, 34304
+# 29, 33309
+
+# Total and mean losses for grades 29-42
+
+print("Grades (29-43) Total and mean losses")
+
+total_mean_losses_29_43 = []
+for cpi, df in zip(cpi_list, df_by_cpi_list):
+    lect_df = df[np.logical_and(df["salary"] > 33309, df["salary"] < 50296)]
+    total_loss_pounds, mean_loss_pounds, mean_loss_percent = total_and_mean_losses(lect_df)
+    total_mean_losses_29_43.append([total_loss_pounds, mean_loss_pounds, mean_loss_percent])
+    print("CPI {:.1f}%  Total loss GBP: {:.3}b".format(cpi/10, total_loss_pounds/1e9), 
+          "Mean loss GBP: {:.1f}k".format(mean_loss_pounds/1e3), 
+          "Mean loss %: {:.3f}".format(mean_loss_percent),
+           "Total number (29-43): {:}".format(lect_df["number"].sum()))
+
+tml_29_43_df = pd.DataFrame(total_mean_losses, 
+                      columns=["Total loss grades 29-43 GBP", "Mean loss grades 29-43 GBP", "Mean loss grades 29-43 %"])
+
+losses_df = losses_df.join(tml_29_43_df)
+#%%
+print("Grades (29-43) quartile losses")
+
+loss_percent_quartiles_29_43_list = []
+
+for cpi, df in zip(cpi_list, df_by_cpi_list):
+    lect_df = df[np.logical_and(df["salary"] > 33309, df["salary"] < 50296)]
+    loss_percent_quartiles = quartile_losses(lect_df, "loss_percent")
+    loss_percent_quartiles_29_43_list.append(loss_percent_quartiles)
+    # print("Quartile losses kGBP:", loss_pounds_quartiles/1e3)
+    print("CPI {:.1f}%  Grades (29-43) Quartile losses %:   ".format(cpi/10), loss_percent_quartiles) 
+
+
+lpcq_29_43_df = pd.DataFrame(loss_percent_quartiles_list, 
+                       columns=["Grades (29-43) % loss Q1", "Grades (29-43) % loss Q2", "Grades (29-43) % loss Q3"])
+
+losses_df = losses_df.join(lpcq_29_43_df)
+
+#%%
+print("Grades (29-43) Quartile losses (pounds)")
+
+loss_pounds_quartiles_29_43_list = []
+
+np.set_printoptions(precision=0)
+for cpi, df in zip(cpi_list, df_by_cpi_list):
+    lect_df = df[np.logical_and(df["salary"] > 33309, df["salary"] < 50296)]
+    loss_pounds_quartiles = quartile_losses(lect_df, "loss_pounds")
+    loss_pounds_quartiles_29_43_list.append(loss_pounds_quartiles)
+    # print("Quartile losses kGBP:", loss_pounds_quartiles/1e3)
+    print("CPI {:.1f}%  Grades (29-43) Quartile losses GBP:   ".format(cpi/10), loss_pounds_quartiles) 
+
+lgbpq_29_43_df = pd.DataFrame(loss_percent_quartiles_list, 
+                       columns=["Grades (29-43) GBP loss Q1", "Grades (29-43) GBP loss Q2", "Grades (29-43) GBP loss Q3"])
+
+losses_df = losses_df.join(lgbpq_29_43_df)
+
+#%%
+print("Lecturer age 37 on 40k losses (pounds)")
+
+np.set_printoptions(precision=0)
+loss_pounds_37_40k_list = []
+loss_percent_37_40k_list = []
+losses_37_40k = []
+for cpi, df in zip(cpi_list, df_by_cpi_list):
+    lect_37_40k_df = df[np.logical_and(df["salary"] >= 37500, df["salary"] <= 47500)]
+    lect_37_40k_df = lect_37_40k_df[np.logical_and(lect_37_40k_df["age"] > 37,lect_37_40k_df["age"]<43)]
+    weight = lect_37_40k_df["number"]/(lect_37_40k_df["number"].sum())
+    loss_pounds_37_40k = (lect_37_40k_df["loss_pounds"] * weight).sum()
+    # loss_pounds_37_40k_list.append(loss_pounds_37_40k)
+    loss_percent_37_40k = (lect_37_40k_df["loss_percent"] * weight).sum()
+    # loss_percent_37_40k_list.append(loss_percent_37_40k)
+    losses_37_40k.append([loss_percent_37_40k, loss_pounds_37_40k])
+    print("CPI {:.1f}%  Lecturer age 37 on 40k losses GBP: {:.0f}".format(cpi/10,loss_pounds_37_40k) 
+        + "   losses %: {:.0f}".format(loss_percent_37_40k) ) 
+
+l_37_40k_df = pd.DataFrame(losses_37_40k, 
+                       columns=["Age 37, 40k % loss", "Age 37, 40k GBP loss"])
+
+losses_df = losses_df.join(l_37_40k_df)
+
+#%%
+
+print("Loss in retirement for those earning under 40k")
+
+total_mean_loss_under_40k_list = []
+for cpi in cpi_list:
+    df = pension_df_under40k[pension_df_under40k['inflation'] == cpi/10]
+    a, b, c = total_and_mean_losses(df)
+    total_mean_loss_under_40k_list.append([a, b, c])
+    print("Total loss GBP: {:.3} b".format(a/1e9), 
+          "Mean loss GBP: {:.1f} k".format(b/1e3), 
+          "Mean loss %: {:.3f}".format(c) )
+
+tml_under_40k_df = pd.DataFrame(total_mean_losses, 
+                      columns=["Total loss grades < 40k bGBP", "Mean loss < 40k GBP", "Mean loss < 40k %"])
+
+losses_df = losses_df.join(tml_under_40k_df)
+
+
+#%%
+
+print("Earning under 40k quartile losses percent")
+
+loss_percent_quartiles_under40k_list = []
+for cpi in cpi_list:
+    df = pension_df_under40k[pension_df_under40k['inflation'] == cpi/10]
+    loss_percent_quartiles = quartile_losses(df, "loss_percent")
+    loss_percent_quartiles_under40k_list.append(loss_percent_quartiles)
+    print("CPI {:.1f}%  Under 40k Quartile losses GBP:   ".format(cpi/10), loss_percent_quartiles) 
+
+lpcq_under_40k_df = pd.DataFrame(loss_percent_quartiles_under40k_list, 
+                       columns=["Under 40k % loss Q1", "Under 40k % loss Q2", "Under 40k % loss Q3"])
+
+losses_df = losses_df.join(lpcq_under_40k_df)
+
+
+#%%
+
+print("Earning under 40k quartile losses GBP")
+
+loss_pounds_quartiles_under40k_list = []
+for cpi in cpi_list:
+    df = pension_df_under40k[pension_df_under40k['inflation'] == cpi/10]
+    loss_pounds_quartiles = quartile_losses(df, "loss_pounds")
+    loss_pounds_quartiles_under40k_list.append(loss_pounds_quartiles)
+    print("CPI {:.1f}%  Under 40k Quartile losses GBP:   ".format(cpi/10), loss_pounds_quartiles) 
+
+lgbpq_under_40k_df = pd.DataFrame(loss_pounds_quartiles_under40k_list, 
+                       columns=["Under 40k GBP loss Q1", "Under 40k GBP loss Q2", "Under 40k GBP loss Q3"])
+
+losses_df = losses_df.join(lgbpq_under_40k_df)
+
 
 #%%
 #Now for the plotting 
@@ -229,7 +445,7 @@ newcmp = colormap_from_rgb_list(cols)
 xlabel_txt = "Percentage loss in retirement, age 66-86"
 ylabel_txt = "Number of active USS members"
 suptitle_txt = "Loss in future USS pension value due to UUK cuts 2022"
-title_txt = "According to USS modeller, lump sum and DC taken as annuity, CPI {}%"
+title_txt = "According to USS modeller, DC taken as annuity, CPI {}%"
 
 for non_zero_nos_df, cpi in zip(non_zero_nos_df_list, cpi_list):
     grouped_df = non_zero_nos_df.groupby(['binned_by_pc_loss','binned_by_salary']).sum().unstack()
@@ -245,7 +461,7 @@ for non_zero_nos_df, cpi in zip(non_zero_nos_df_list, cpi_list):
     ax.get_figure().suptitle(suptitle_txt, fontsize='larger', x=0.55, y=0.93)
     ax.set_title(title_txt.format(cpi/10), fontsize='smaller', x=0.49)
     ax.get_figure().tight_layout()
-    ax.get_figure().savefig(f'uuk_cuts_salary_stack_cpi{cpi:}.pdf')
+    ax.get_figure().savefig(f'salary_stack_cpi{cpi:}.pdf')
     # ax.annotate("A", (0.5,0.5), xycoords='axes fraction')
 
 
@@ -266,7 +482,7 @@ for non_zero_nos_df, cpi in zip(non_zero_nos_df_list, cpi_list):
     ax.get_figure().suptitle(suptitle_txt, fontsize='larger',  x=0.55, y=0.93)
     ax.set_title(title_txt.format(cpi/10), fontsize='smaller', x=0.49)
     ax.get_figure().tight_layout()
-    ax.get_figure().savefig(f'uuk_cuts_age_stack_cpi{cpi:}.pdf')
+    ax.get_figure().savefig(f'age_stack_cpi{cpi:}.pdf')
 
 
 #%%
@@ -275,7 +491,7 @@ for non_zero_nos_df, cpi in zip(non_zero_nos_df_list, cpi_list):
 xlabel_gbp_txt = "Loss in retirement, age 66-86 [GBP, today's money']"
 ylabel_txt = "Number of active USS members"
 suptitle_txt = "Loss in future USS pension value due to UUK cuts 2022"
-title_txt = "According to USS modeller, lump sum and DC taken as annuity, CPI {}%"
+title_txt = "According to USS modeller, DC taken as annuity, CPI {}%"
 
 for non_zero_nos_df, cpi in zip(non_zero_nos_df_list, cpi_list):
     grouped_df = non_zero_nos_df.groupby(['binned_by_pounds_loss','binned_by_salary']).sum().unstack()
@@ -291,7 +507,7 @@ for non_zero_nos_df, cpi in zip(non_zero_nos_df_list, cpi_list):
     ax.get_figure().suptitle(suptitle_txt, fontsize='larger', x=0.55, y=0.93)
     ax.set_title(title_txt.format(cpi/10), fontsize='smaller', x=0.49)
     ax.get_figure().tight_layout()
-    ax.get_figure().savefig(f'uuk_cuts_gbp_salary_stack_cpi{cpi:}.pdf')
+    ax.get_figure().savefig(f'gbp_salary_stack_cpi{cpi:}.pdf')
 
 
 #%%
@@ -311,7 +527,7 @@ for non_zero_nos_df, cpi in zip(non_zero_nos_df_list, cpi_list):
     ax.get_figure().suptitle(suptitle_txt, fontsize='larger',  x=0.55, y=0.93)
     ax.set_title(title_txt.format(cpi/10), fontsize='smaller', x=0.49)
     ax.get_figure().tight_layout()
-    ax.get_figure().savefig(f'uuk_cuts_gbp_age_stack_cpi{cpi:}.pdf')#%%
+    ax.get_figure().savefig(f'gbp_age_stack_cpi{cpi:}.pdf')#%%
 
 #%%
 # Now for side-by-side for those under 40k
@@ -324,20 +540,8 @@ cols_40k = [(253,217,102),
 newcmp_40k = colormap_from_rgb_list(cols_40k)
 
 suptitle_txt_40k = "Loss in future USS pension value due to UUK cuts 2022 \n Staff with salary under " +chr(163)+ "{}k"
-title_txt_40k = "According to USS modeller, lump sum and DC taken as annuity"
+title_txt_40k = "According to USS modeller, DC taken as annuity"
 
-salary_thresh = 40000
-
-pension_df_under40k = pension_df[pension_df['salary'] < salary_thresh].copy()
-
-pension_df_under40k['binned_by_pc_loss'] = pd.cut(pension_df_under40k['loss_percent'], 
-                                               bins=pc_bin_edges, right=False, labels=pc_bin_labels)
-pension_df_under40k['inflation'] *= 100
-pension_df_under40k['inflation'] = pension_df_under40k['inflation'].round(1)
-
-pension_df_under40k_binned = bin_and_remove_zeros(pension_df_under40k.copy())
-
-pension_df_under40k_binned_grouped = pension_df_under40k_binned.groupby(['binned_by_pc_loss','inflation']).sum().unstack()
 ax_40k = pension_df_under40k_binned_grouped.plot.bar(y='number', 
                     xlabel=xlabel_txt, ylabel=ylabel_txt, 
                     ylim=(0,25000), colormap=newcmp_40k)
@@ -350,24 +554,17 @@ ax_40k.grid(which='minor', axis='y', alpha=0.25)
 ax_40k.get_figure().suptitle(suptitle_txt_40k.format(salary_thresh//1000), fontsize='larger',  x=0.55, y=0.93)
 ax_40k.set_title(title_txt_40k, fontsize='smaller', x=0.49)
 ax_40k.get_figure().tight_layout()
-ax_40k.get_figure().savefig('uuk_cuts_under_{}k_all_cpi.pdf'.format(salary_thresh//1000))
+ax_40k.get_figure().savefig('{}k_all_cpi.pdf'.format(salary_thresh//1000))
 
-#%%
-
-print("Loss in retirement for those earning under 40k")
-for cpi in [2.5, 2.8, 3.0]:
-    df = pension_df_under40k[pension_df_under40k['inflation'] == cpi]
-    a, b, c = total_and_mean_losses(df)
-    print("Total loss GBP: {:.3} b".format(a/1e9), 
-          "Mean loss GBP: {:.1f} k".format(b/1e3), 
-          "Mean loss %: {:.3f}".format(c) )
 
 #%%
 
 # And the saving of data files
 
-for df_by_cpi, non_zero_nos_df in zip(df_by_cpi_list, non_zero_nos_df_list):
+for cpi, df_by_cpi, non_zero_nos_df in zip(cpi_list, df_by_cpi_list, non_zero_nos_df_list):
 
-    df_by_cpi.to_csv(f'uss_pension_pc_loss_cpi{cpi:}.csv', index=False)
-    non_zero_nos_df.to_csv(f'uss_binned_pc_loss_age_salary_cpi{cpi:}.csv', index=False)
+    df_by_cpi.to_csv(f'heatmap_modeller_alldata_cpi{cpi:}.csv', index=False)
+    non_zero_nos_df.to_csv(f'heatmap_modeller_binnedlosses_cpi{cpi:}.csv', index=False)
 
+
+losses_df.to_csv("heatmap_modeller_losses_cpi_25_28_30_digest.csv", index=False)
